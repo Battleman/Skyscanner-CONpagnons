1ab07c1791aae6da03692d9736793101e50c0103
Thierry Treyer <zehkae@gmail.com>
2017-11-12 11:50:35 +0100

    Yay

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from olivier import suggest_airport
 from cities import get_most_likely_cities_file
 from randomWalk import randomWalk
+from newgetters import fast_getter, heavy_getter
 
 from flask import Flask, request, render_template, jsonify
 app = Flask("Skyscanner-CONpagnons")
@@ -21,7 +22,16 @@ def fromPictureURL():
 def suggestAirport(needle):
     return jsonify(suggest_airport(needle))
 
-@app.route('/walk/<depart>/<int:budget>/<from_date>/<to_date>/<int:escale_length>')
-def walk(depart, budget, from_date, to_date, escale_length):
-    return jsonify(randomWalk(depart, budget, from_date, to_date, dayspercity=escale_length))
+@app.route('/walk/<depart>/<int:budget>/<from_date>/<int:escale_length>')
+def walk(depart, budget, from_date, escale_length):
+    while True:
+        travel = fast_getter(depart, budget, from_date, dayspercity=escale_length, marge=0.05)
+        if len(travel) > 0:
+            break
 
+    return jsonify(travel)
+
+@app.route('/heavyWalk', methods=['POST'])
+def heavy():
+    print(request.data)
+    return jsonify(heavy_getter(request.get_json(force=True)))
